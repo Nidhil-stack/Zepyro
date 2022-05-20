c1866fdf2c3bad7371fac85451e9c07e79927d65
Nidhil-stack <76472442+Nidhil-stack@users.noreply.github.com>
2022-05-20 22:41:57 +0200

    commented the hallEffect module

--- libs/hallSensor/hallSensor.py
+++ libs/hallSensor/hallSensor.py
@@ -1,15 +1,15 @@
 import gpio
 import adc
 
-class hallSensor(int):
-    def __init__(self, pin):
+class hallSensor(int):                                      #class to read the hall sensor and transform its analog value to a digital value
+    def __init__(self, pin):                                #constructor, saves the gpio number and initializes the gpio pin as input
         self.pin = pin
         gpio.mode(self.pin, INPUT)
 
-    def read(self):
-        value = adc.read(self.pin)
+    def read(self):                                         #read the analog value of the hall sensor and returns a digital one
+        value = adc.read(self.pin)                          #read the analog value
 
-        if value > 2000:
+        if value > 2000:                                    #add some hysteresis to the value for stability reasons
             self.old = 1
             return 1
         else:
@@ -17,4 +17,4 @@ class hallSensor(int):
                 self.old = 0
                 return 0
             else:
-                return self.old
+                return self.old                             #return the last value if the value is not stable
